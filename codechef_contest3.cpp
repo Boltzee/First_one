8f426d563ca554bfe67e8aeec8f33cb83b980d95
Kiran Tirunagiri <>
2021-01-14 10:07:49 +0530

    new files for the directory

--- codechef_contest3.cpp
+++ codechef_contest3.cpp
@@ -5,13 +5,15 @@ int main(){
 	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 	int T;cin>>T;
 	while(T--){
-		int n,m;
-		cin>>n>>m;
-		int a[n+1][m+1];
-		for(int i=1;i<n+1;i++){
-			for(int j=1;j<m+1;j++){
-				cin>>a[i][j];
-			}
+		int n,k;cin>>n>>k;
+		if(n>k){
+			cout<<"-1\n";continue;
 		}
+		else if (n==k){
+			string s(n,'a');
+			cout<<s<<"\n";co
+		}
+		
 	}
+	return 0;
 }
